8082b36e43e2c9342ebd9d9bdc26a696f5892178
aurelien <aurelien@kickersoft.com>
2025-08-07 01:43:12 -0500

    Update PlantItem styles, add price badge, and enhance Cart with dynamic title update

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import '../styles/Cart.css'
 
 const Cart = ({cart, updateCart}) => {
     const [isOpen, setIsOpen] = useState(true)
-    const total = cart.reduce(
-        (acc, plantType) => acc + plantType.amount * plantType.price,
+    const items = Object.keys(cart)
+    const total = items.reduce(
+        (acc, item) => acc + cart[item].amount * cart[item].price,
         0
     )
+    useEffect(() => {
+        document.title = `LMJ: ${total}€ d'achats`
+    }, [total])
+
     return isOpen ? (
         <div className='lmj-cart'>
             <button
