3cb60ce155c398a37f27e0553b0def913bbaa233
aurelien <aurelien@kickersoft.com>
2025-08-07 00:18:39 -0500

    Refactor Cart component to add toggle functionality, enhance layout structure in App, and include new plant assets

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Banner from './components/Banner'
 import Cart from './components/Cart'
 import Footer from './components/Footer'
 import ShoppingList from './components/ShoppingList'
-
 import logo from './assets/logo.png'
+import './styles/Layout.css'
 
 
 const App = () => {
@@ -13,8 +13,10 @@ const App = () => {
                 <img src={logo} alt='La maison jungle' className='lmj-logo' />
                 <h1 className='lmj-title'>La maison jungle</h1>
             </Banner>
-            {/* <Cart /> */}
-            <ShoppingList />
+            <div className='lmj-layout-inner'>
+                <Cart />
+                <ShoppingList />
+            </div>
             <Footer />
         </div>
     )
