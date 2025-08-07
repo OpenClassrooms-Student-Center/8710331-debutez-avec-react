ff235a786b9fc8c83742314380383181724911b9
aurelien <aurelien@kickersoft.com>
2025-08-06 19:00:32 -0500

    Add Logo and style for Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,18 @@
+import '../styles/Banner.css'
+import logo from '../images/logo.png'
+
 const Banner = () => {
-  return <h1>🌿 La maison jungle</h1>
+  const title = 'La maison jungle'
+  return (
+      <div className="banner">
+        <img
+            src={logo}
+            alt="Logo La maison jungle"
+            className="banner-logo"
+        />
+        <h1 className="banner-title">{title}</h1>
+      </div>
+  )
 }
 
 export default Banner
