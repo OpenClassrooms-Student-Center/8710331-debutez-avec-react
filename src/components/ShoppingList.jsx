c417ed4758edabd64824d18d80d18b757e045abd
aurelien <aurelien@kickersoft.com>
2025-08-06 23:48:05 -0500

    Add Footer component, improve PlantItem interactivity, and replace CareScale icons with images

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -19,7 +19,7 @@ const ShoppingList = () => {
 			<ul className='lmj-plant-list'>
 				{plantList.map(({ id, cover, name, water, light }) => (
 					<PlantItem
-						id={id}
+						key={id}
 						cover={cover}
 						name={name}
 						water={water}
