3455352a496315426f47581a468c8d0efe300672
aurelien <aurelien@kickersoft.com>
2025-10-21 23:37:46 -0500

    Remove Cart.css and related styling from Cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,9 @@
-import '../styles/Cart.css'
 const Cart = () => {
   const monsteraPrice = 8;
   const ivyPrice = 10;
   const flowerPrice = 15;
   return (
-    <div className={ "cart"}>
+    <div>
       <h2>Panier</h2>
       <ul>
         <li>Monstera : {monsteraPrice}€</li>
